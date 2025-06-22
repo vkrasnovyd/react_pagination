154e97948b2d86530455c382376b32e5c73718c1
vkrasnovyd <v.krasnovyd@proton.me>
2025-06-22 17:01:31 +0200

    Move state updating to utils

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import React from 'react';
 
 import './App.css';
 
-import { getNumbers, getPageItems } from './utils';
+import { getNumbers, getPageItems, usePaginationParams } from './utils';
 import { Info } from './components/Info';
 import { PageItems } from './components/PageItems';
 import { Pagination } from './components/Pagination';
 import { PerPageSelector } from './components/PerPageSelector';
-import { PerPage, perPageOptions } from './types/PerPage';
 
 const items = getNumbers(1, 42).map(n => `Item ${n}`);
 
 export const App: React.FC = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const perPageParamRaw = parseInt(searchParams.get('perPage') || '5', 10);
-  const pageNumberParamRaw = parseInt(searchParams.get('page') || '1', 10);
-
-  const perPageParam = perPageOptions.includes(perPageParamRaw as PerPage)
-    ? (perPageParamRaw as PerPage)
-    : 5;
-  const [maxPagesCount, setMaxPagesCount] = useState<number>(
-    Math.ceil(items.length / perPageParam),
-  );
-  const pageNumberParam = Math.max(
-    1,
-    Math.min(pageNumberParamRaw, maxPagesCount),
-  );
-
-  const [perPage, setPerPage] = useState<PerPage>(perPageParam);
-  const [pageNumber, setPageNumber] = useState<number>(pageNumberParam);
+  const {
+    perPage,
+    pageNumber,
+    maxPagesCount,
+    setPerPage,
+    setPageNumber,
+    setMaxPagesCount,
+  } = usePaginationParams(items.length);
 
   const paginatedItems = getPageItems(items, { perPage, pageNumber });
-
-  useEffect(() => {
-    setSearchParams({ perPage: String(perPage), page: String(pageNumber) });
-  }, [pageNumber, perPage, setSearchParams]);
-
-  useEffect(() => {
-    setMaxPagesCount(Math.ceil(items.length / perPage));
-  }, [perPage]);
 
   return (
     <div className="container">
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
+import { useSearchParams } from 'react-router-dom';
+import { PerPage, perPageOptions } from './types/PerPage';
+import { useEffect, useState } from 'react';
+
 export function getNumbers(from: number, to: number): number[] {
   const numbers = [];
 
@@ -19,3 +23,41 @@ export function getPageItems(
 
   return [...itemsList].slice(firstElementId, lastElementId + 1);
 }
+
+export const usePaginationParams = (itemsCount: number) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const perPageParamRaw = parseInt(searchParams.get('perPage') || '5', 10);
+  const pageNumberParamRaw = parseInt(searchParams.get('page') || '1', 10);
+
+  const perPageParam = perPageOptions.includes(perPageParamRaw as PerPage)
+    ? (perPageParamRaw as PerPage)
+    : 5;
+  const [maxPagesCount, setMaxPagesCount] = useState<number>(
+    Math.ceil(itemsCount / perPageParam),
+  );
+  const pageNumberParam = Math.max(
+    1,
+    Math.min(pageNumberParamRaw, maxPagesCount),
+  );
+
+  const [perPage, setPerPage] = useState<PerPage>(perPageParam);
+  const [pageNumber, setPageNumber] = useState<number>(pageNumberParam);
+
+  useEffect(() => {
+    setSearchParams({ perPage: String(perPage), page: String(pageNumber) });
+  }, [pageNumber, perPage, setSearchParams]);
+
+  useEffect(() => {
+    setMaxPagesCount(Math.ceil(itemsCount / perPage));
+  }, [perPage, itemsCount]);
+
+  return {
+    perPage,
+    pageNumber,
+    maxPagesCount,
+    setPerPage,
+    setPageNumber,
+    setMaxPagesCount,
+  };
+};
