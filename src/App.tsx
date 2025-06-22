412549df74319b76f4db7a4f73173249c0b1fbec
vkrasnovyd <v.krasnovyd@proton.me>
2025-06-22 11:38:46 +0200

    Split code further

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
+import classNames from 'classnames';
 import React, { useState } from 'react';
 import './App.css';
 import { getNumbers } from './utils';
-import classNames from 'classnames';
 
 const items = getNumbers(1, 42).map(n => `Item ${n}`);
 
-interface Props1 {
+interface PageItemsProps {
   perPage: number;
   pageNumber: number;
 }
 
-interface Props2 extends Props1 {
+interface PageItemsInfoProps extends PageItemsProps {
   itemsList: string[];
+}
+
+interface PaginationProps {
+  pageNumber: number;
+  maxPagesCount: number;
+  onClick: (newValue: number) => void;
 }
 
 const perPageOptions = [3, 5, 10, 20] as const;
 
 type PerPage = (typeof perPageOptions)[number];
 
+interface PerPageSelectorProps {
+  itemsList: string[];
+  perPage: number;
+  perPageSetter: (newValue: PerPage) => void;
+  maxPagesSetter: (newValue: number) => void;
+  pageNumberSetter: (newValue: number) => void;
+}
+
 function getPageItems(
   itemsList: string[],
-  { perPage, pageNumber }: Props1,
+  { perPage, pageNumber }: PageItemsProps,
 ): string[] {
   const maxPages = Math.ceil(itemsList.length / perPage);
   const firstElementId = (pageNumber - 1) * perPage;
@@ -30,11 +44,63 @@ function getPageItems(
   return [...itemsList].slice(firstElementId, lastElementId + 1);
 }
 
-// const Pagination = () => {};
+const Pagination = ({
+  pageNumber,
+  maxPagesCount,
+  onClick,
+}: PaginationProps) => (
+  <ul className="pagination">
+    <li
+      className={classNames('page-item', {
+        disabled: pageNumber === 1,
+      })}
+    >
+      <a
+        data-cy="prevLink"
+        className="page-link"
+        href="#prev"
+        aria-disabled={pageNumber === 1}
+        onClick={() => {
+          onClick(pageNumber - 1);
+        }}
+      >
+        «
+      </a>
+    </li>
+    {getNumbers(1, maxPagesCount).map(n => (
+      <li
+        className={classNames('page-item', { active: n === pageNumber })}
+        key={`page_${n}`}
+        onClick={() => {
+          onClick(n);
+        }}
+      >
+        <a data-cy="pageLink" className="page-link" href={`#${n}`}>
+          {n}
+        </a>
+      </li>
+    ))}
+    <li
+      className={classNames('page-item', {
+        disabled: pageNumber === maxPagesCount,
+      })}
+    >
+      <a
+        data-cy="nextLink"
+        className="page-link"
+        href="#next"
+        aria-disabled={pageNumber === maxPagesCount}
+        onClick={() => {
+          onClick(pageNumber + 1);
+        }}
+      >
+        »
+      </a>
+    </li>
+  </ul>
+);
 
-// const PaginationElement = (pageNumber: number) => ();
-
-const Info = ({ itemsList, perPage, pageNumber }: Props2) => {
+const Info = ({ itemsList, perPage, pageNumber }: PageItemsInfoProps) => {
   const maxPages = Math.ceil(itemsList.length / perPage);
   const normlizedPageNumber = Math.max(1, Math.min(pageNumber, maxPages));
   const firstElementId = (normlizedPageNumber - 1) * perPage + 1;
@@ -51,6 +117,54 @@ const Info = ({ itemsList, perPage, pageNumber }: Props2) => {
   );
 };
 
+const PerPageSelector = ({
+  itemsList,
+  perPage,
+  perPageSetter,
+  maxPagesSetter,
+  pageNumberSetter,
+}: PerPageSelectorProps) => (
+  <div className="form-group row">
+    <div className="col-3 col-sm-2 col-xl-1">
+      <select
+        data-cy="perPageSelector"
+        id="perPageSelector"
+        className="form-control"
+        onChange={event => {
+          const newPerPageValue = (e: React.ChangeEvent<HTMLSelectElement>) =>
+            +e.target.value as PerPage;
+
+          perPageSetter(newPerPageValue(event));
+          maxPagesSetter(Math.ceil(itemsList.length / newPerPageValue(event)));
+          pageNumberSetter(1);
+        }}
+      >
+        {perPageOptions.map((option: PerPage) => {
+          return (
+            <option value={option} key={option} selected={option === perPage}>
+              {option}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+
+    <label htmlFor="perPageSelector" className="col-form-label col">
+      items per page
+    </label>
+  </div>
+);
+
+const PageItems = ({ itemsList }: { itemsList: string[] }) => (
+  <ul>
+    {itemsList.map(item => (
+      <li data-cy="item" key={item}>
+        {item}
+      </li>
+    ))}
+  </ul>
+);
+
 export const App: React.FC = () => {
   const [perPage, setPerPage] = useState<PerPage>(5);
   const [pageNumber, setPageNumber] = useState<number>(1);
@@ -62,97 +176,20 @@ export const App: React.FC = () => {
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
-
       <Info itemsList={items} perPage={perPage} pageNumber={pageNumber} />
-
-      <div className="form-group row">
-        <div className="col-3 col-sm-2 col-xl-1">
-          <select
-            data-cy="perPageSelector"
-            id="perPageSelector"
-            className="form-control"
-            onChange={event => {
-              setPerPage(+event.target.value as PerPage);
-              setMaxPagesCount(Math.ceil(items.length / perPage));
-            }}
-          >
-            {perPageOptions.map(option => {
-              return (
-                <option
-                  value={option}
-                  key={option}
-                  selected={option === perPage}
-                >
-                  {option}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-
-        <label htmlFor="perPageSelector" className="col-form-label col">
-          items per page
-        </label>
-      </div>
-
-      {/* Move this markup to Pagination */}
-      <ul className="pagination">
-        <li
-          className={classNames('page-item', {
-            disabled: pageNumber === 1,
-          })}
-        >
-          <a
-            data-cy="prevLink"
-            className="page-link"
-            href="#prev"
-            aria-disabled={pageNumber === 1}
-            onClick={() => {
-              setPageNumber(currentPageNumber => currentPageNumber - 1);
-            }}
-          >
-            «
-          </a>
-        </li>
-        {/* <PaginationElement /> */}
-        {getNumbers(1, maxPagesCount).map(n => (
-          <li
-            className={classNames('page-item', { active: n === pageNumber })}
-            key={`page_${n}`}
-            onClick={() => {
-              setPageNumber(n);
-            }}
-          >
-            <a data-cy="pageLink" className="page-link" href={`#${n}`}>
-              {n}
-            </a>
-          </li>
-        ))}
-        <li
-          className={classNames('page-item', {
-            disabled: pageNumber === maxPagesCount,
-          })}
-        >
-          <a
-            data-cy="nextLink"
-            className="page-link"
-            href="#next"
-            aria-disabled={pageNumber === maxPagesCount}
-            onClick={() => {
-              setPageNumber(currentPageNumber => currentPageNumber + 1);
-            }}
-          >
-            »
-          </a>
-        </li>
-      </ul>
-      <ul>
-        {paginatedItems.map(item => (
-          <li data-cy="item" key={item}>
-            {item}
-          </li>
-        ))}
-      </ul>
+      <PerPageSelector
+        itemsList={items}
+        perPage={perPage}
+        perPageSetter={setPerPage}
+        maxPagesSetter={setMaxPagesCount}
+        pageNumberSetter={setPageNumber}
+      />
+      <Pagination
+        pageNumber={pageNumber}
+        maxPagesCount={maxPagesCount}
+        onClick={setPageNumber}
+      />
+      <PageItems itemsList={paginatedItems} />
     </div>
   );
 };
